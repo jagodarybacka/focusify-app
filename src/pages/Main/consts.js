2db3a7ddb72a121d9999c1bfe80d97b33803ac6b
jagodarybacka <jagoda.roza.rybacka@gmail.com>
2020-12-05 15:27:45 +0100

    bugfix: Fix responsiveness

--- src/pages/Main/consts.js
+++ src/pages/Main/consts.js
@@ -1,6 +1,6 @@
 export const DEFAULT_WORK_TIME = 45;
 export const DEFAULT_REST_TIME = 15;
-export const MIN_TIME = 5;
+export const MIN_TIME = 1;
 export const MAX_TIME = 120;
 
 export const STAGE = {
