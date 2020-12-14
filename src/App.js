b3d8efc0f2710206ba645cbf69ffc6ee3c6d6bd8
jagodarybacka <jagoda.roza.rybacka@gmail.com>
2020-12-14 22:30:48 +0100

    feature: Button animation, Routes animation, removed Link

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { useTransition, animated } from 'react-spring';
 import {
   Switch,
   Route,
-  BrowserRouter as Router
+  useLocation
 } from 'react-router-dom';
 
 import TokenContext from 'context/token';
@@ -17,32 +18,37 @@ function App() {
   const [ token, setToken ] = useState('');
   const [ playlists, setPlaylists ] = useState([]);
 
+  const location = useLocation();
+  const transitions = useTransition(location, current => current.pathname, {
+    from: { opacity: 0, position: 'absolute' },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
+  });
+
   useEffect(() => {
     const currentToken = getToken();
 
     currentToken && setToken(currentToken);
   }, []);
 
-  return (
-    <Router>
-      <div className="App">
-        <TokenContext.Provider value={token}>
-          <Switch>
-            <Route path="/setup">
-              <Setup
-                setPlaylists={setPlaylists}/>
-            </Route>
-            <Route path="/player">
-              <Player playlists={playlists}/>
-            </Route>
-            <Route path="/">
-              <Intro />
-            </Route>
-          </Switch>
-        </TokenContext.Provider>
-      </div>
-    </Router>
-  );
+  return transitions.map(({ item: itemLocation, props, key }) => (
+    <animated.div key={key} style={props} className="App">
+      <TokenContext.Provider value={token}>
+        <Switch location={itemLocation}>
+          <Route path="/setup">
+            <Setup
+              setPlaylists={setPlaylists}/>
+          </Route>
+          <Route path="/player">
+            <Player playlists={playlists}/>
+          </Route>
+          <Route exact path="/">
+            <Intro />
+          </Route>
+        </Switch>
+      </TokenContext.Provider>
+    </animated.div>
+  ));
 }
 
 export default App;
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSpring, animated } from 'react-spring';
 import clssnames from 'classnames';
 import './styles.scss';
 
 export default function Button({ onClick, isDisabled, isHidden, isAccent, children }) {
+  const [ { scale }, setSpring ] = useSpring(() => ({ scale: 1 }));
   const classes = clssnames({
     'Button': true,
     'is-disabled': isDisabled,
@@ -12,10 +14,15 @@ export default function Button({ onClick, isDisabled, isHidden, isAccent, childr
   });
 
   return (
-    <button
+    <animated.button
       className={classes}
       onClick={onClick}
-      disabled={isDisabled}>{children}</button>
+      disabled={isDisabled}
+      onMouseEnter={() => setSpring({ scale: 1.05 })}
+      onMouseLeave={() => setSpring({ scale: 1 })}
+      style={{ transform: scale.interpolate(s => `scale(${ s })`) }}>
+      {children}
+    </animated.button>
   );
 }
 
